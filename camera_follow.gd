3d78003efa0fe41b9b26e0faf6058b9ae9e130e3
Martin Delille <martin@delille.org>
2025-11-27 23:51:35 +0100

    First commit

---
+++ camera_follow.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@export var target_path: NodePath
+var target: Node3D
+
+func _ready() -> void:
+	if target_path:
+		target = get_node(target_path)
+
+func _process(_delta: float) -> void:
+	if target:
+		global_position = target.global_position
